d62886b45707fa3e46286a3979261499aaf87723
phuchng <hongphucng63@gmail.com>
2021-05-19 21:18:48 -0400

    Distribute bws package

---
+++ R/bws.R
@@ -0,0 +1,31 @@
+#' Bayesian Weighted Sums
+#'
+#' This function fits a Bayesian Weighted Sums as described in
+#' Bayesian Weighted Sums: A Flexible Approach to Estimate Summed Mixture Effects.
+#' Ghassan B. Hamra 1, Richard F. Maclehose, Lisa Croen, Elizabeth M. Kauffman
+#' and Craig Newschaffer. 2021. International Journal of Environmental Research and Public Health.
+#'
+#' @param iter Number of Hamiltonian Monte Carlo iterations
+#' @param y    Am n-vector of outcomes
+#' @param X    An n-by-p matrix of mixtures to be weightedly summed
+#' @param Z    Default NULL. A matrix of confounders whose linear effects are estimated
+#' @param alpha A p-vector of hyperparameters for the Dirichlet prior on the weights.
+#' Default to be a vector of 1's.
+#' @param ... Additional arguments for `rstan::sampling`
+#' @return An object of class `stanfit` returned by `rstan::sampling`
+#' @export
+bws <- function(iter, y, X, Z=NULL, alpha=NULL, ...) {
+  N <- length(y)
+  if (N == 0) stop("Sample size of zero")
+  if (length(X) > 0 & is.vector(X)) X <- array(X, dim = c(N, 1))
+  P <- ncol(X)
+  if (P == 0) stop("No predictors")
+  if (is.null(alpha)) alpha <- rep(1, P)
+  if (is.null(Z)) Z <- array(0, dim=c(N,1))
+  K <- ncol(Z)
+
+  data <- list(y=y, x=X, z=Z, N=N, P=P, K=K, alpha=alpha)
+  fit <- rstan::sampling(stanmodels$bws, data = data, ...)
+
+  return(fit)
+}
---
+++ inst/stan/bws.stan
@@ -0,0 +1,28 @@
+// The input data is a vector 'y' of length 'N', and matrix 'X' of dimension 'N x P'.
+data {
+  int<lower=1> N;
+  int<lower=1> P;
+  int<lower=1> K;
+  vector[N] y;
+  matrix[N, P] x;
+  matrix[N, K] z;
+  vector<lower=0>[P] alpha;  // Weights prior
+}
+
+parameters {
+  real theta0;  // Regression intercept
+  real theta1;  // Regression slope
+  vector[K] beta;  // Covariates coefficients
+  simplex[P] w;  // Weights
+  real<lower=0> sigma;
+}
+
+model {
+  theta1 ~ normal(0, 100);   // vague prior
+  beta ~ normal(0, 100);
+  w ~ dirichlet(alpha);  // prior
+  y ~ normal(theta0 + theta1 * (x * w) + z * beta, sigma);  // likelihood
+}
+
+
+
